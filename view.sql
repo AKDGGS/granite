ea7c6265a92da12ee1e60030ff300f7e603d1b6b
Christopher Ramey <christopher.ramey@alaska.gov>
2013-11-13 19:34:25 +0000

    Fixed bug with well_geom view. Improved formatting.

--- view.sql
+++ view.sql
@@ -5,201 +5,181 @@ SET CLIENT_MIN_MESSAGES TO WARNING;
 
 CREATE OR REPLACE VIEW inventory_geom_precision AS (
 	-- Borehole Point
-	SELECT ib.inventory_id, p.geom
+	(SELECT ib.inventory_id, p.geom
 	FROM inventory_borehole AS ib
 	JOIN borehole_point AS bp ON bp.borehole_id = ib.borehole_id
 	JOIN point AS p ON p.point_id = bp.point_id
-	WHERE p.geom IS NOT NULL
+	WHERE p.geom IS NOT NULL)
 
 	UNION ALL
 
 	-- Outcrop Point
-	SELECT io.inventory_id, p.geom
+	(SELECT io.inventory_id, p.geom
 	FROM inventory_outcrop AS io
 	JOIN outcrop_point AS op ON op.outcrop_id = io.outcrop_id
 	JOIN point AS p ON p.point_id = op.point_id
-	WHERE p.geom IS NOT NULL
+	WHERE p.geom IS NOT NULL)
 
 	UNION ALL
 
 	-- Well Point
-	SELECT iw.inventory_id, p.geom
+	(SELECT iw.inventory_id, p.geom
 	FROM inventory_well AS iw
 	JOIN well_point AS wp ON wp.well_id = iw.well_id
 	JOIN point AS p ON p.point_id = wp.point_id
-	WHERE p.geom IS NOT NULL
+	WHERE p.geom IS NOT NULL)
 
 	UNION ALL
 
 	-- Outcrop Place
-	SELECT io.inventory_id, p.geom
+	(SELECT io.inventory_id, p.geom
 	FROM inventory_outcrop AS io
 	JOIN outcrop_place AS op ON op.outcrop_id = io.outcrop_id
 	JOIN place AS p ON p.place_id = op.place_id
-	WHERE p.geom IS NOT NULL
+	WHERE p.geom IS NOT NULL)
 
 	UNION ALL
 
 	-- Well Place
-	SELECT iw.inventory_id, p.geom
+	(SELECT iw.inventory_id, p.geom
 	FROM inventory_well AS iw
 	JOIN well_place AS wp ON wp.well_id = iw.well_id
 	JOIN place AS p ON p.place_id = wp.place_id
-	WHERE p.geom IS NOT NULL
+	WHERE p.geom IS NOT NULL)
 
 	UNION ALL
 
 	-- Outcrop PLSS
-	SELECT io.inventory_id, p.geom
+	(SELECT io.inventory_id, p.geom
 	FROM inventory_outcrop AS io
 	JOIN outcrop_plss AS op ON op.outcrop_id = io.outcrop_id
 	JOIN plss AS p ON p.plss_id = op.plss_id
-	WHERE p.geom IS NOT NULL AND township IS NOT NULL
+	WHERE p.geom IS NOT NULL AND township IS NOT NULL)
 
 	UNION ALL
 
 	-- Well PLSS
-	SELECT iw.inventory_id, p.geom
+	(SELECT iw.inventory_id, p.geom
 	FROM inventory_well AS iw
 	JOIN well_plss AS wp ON wp.well_id = iw.well_id
 	JOIN plss AS p ON p.plss_id = wp.plss_id
-	WHERE p.geom IS NOT NULL AND township IS NOT NULL
+	WHERE p.geom IS NOT NULL AND township IS NOT NULL)
 
 	UNION ALL
 
 	-- Borehole Quadrangle
-	SELECT ib.inventory_id, q.geom
+	(SELECT ib.inventory_id, q.geom
 	FROM inventory_borehole AS ib
 	JOIN borehole_quadrangle AS bq ON bq.borehole_id = ib.borehole_id
 	JOIN quadrangle AS q ON q.quadrangle_id = bq.quadrangle_id
-	WHERE q.geom IS NOT NULL AND q.scale = 63360
+	WHERE q.geom IS NOT NULL AND q.scale = 63360)
 
 	UNION ALL
 
 	-- Outcrop Quadrangle
-	SELECT io.inventory_id, q.geom
+	(SELECT io.inventory_id, q.geom
 	FROM inventory_outcrop AS io
 	JOIN outcrop_quadrangle AS oq ON oq.outcrop_id = io.outcrop_id
 	JOIN quadrangle AS q ON q.quadrangle_id = oq.quadrangle_id
-	WHERE q.geom IS NOT NULL AND q.scale = 63360
+	WHERE q.geom IS NOT NULL AND q.scale = 63360)
 
 	UNION ALL
 
 	-- Well Quadrangle
-	SELECT iw.inventory_id, q.geom
+	(SELECT iw.inventory_id, q.geom
 	FROM inventory_well AS iw
 	JOIN well_quadrangle AS wq ON wq.well_id = iw.well_id
 	JOIN quadrangle AS q ON q.quadrangle_id = wq.quadrangle_id
-	WHERE q.geom IS NOT NULL AND q.scale = 63360
+	WHERE q.geom IS NOT NULL AND q.scale = 63360)
 );
 
 
 CREATE OR REPLACE VIEW inventory_geom AS (
-	-- Borehole Point
-	SELECT ib.inventory_id, p.geom
-	FROM inventory_borehole AS ib
-	JOIN borehole_point AS bp ON bp.borehole_id = ib.borehole_id
-	JOIN point AS p ON p.point_id = bp.point_id
-	WHERE p.geom IS NOT NULL
-
-	UNION ALL
-
-	-- Outcrop Point
-	SELECT io.inventory_id, p.geom
-	FROM inventory_outcrop AS io
-	JOIN outcrop_point AS op ON op.outcrop_id = io.outcrop_id
-	JOIN point AS p ON p.point_id = op.point_id
-	WHERE p.geom IS NOT NULL
-
-	UNION ALL
-
-	-- Well Point
-	SELECT iw.inventory_id, p.geom
-	FROM inventory_well AS iw
-	JOIN well_point AS wp ON wp.well_id = iw.well_id
-	JOIN point AS p ON p.point_id = wp.point_id
-	WHERE p.geom IS NOT NULL
-
-	UNION ALL
-
-	-- Outcrop Place
-	SELECT io.inventory_id, p.geom
-	FROM inventory_outcrop AS io
-	JOIN outcrop_place AS op ON op.outcrop_id = io.outcrop_id
-	JOIN place AS p ON p.place_id = op.place_id
-	WHERE p.geom IS NOT NULL
-
-	UNION ALL
-
-	-- Well Place
-	SELECT iw.inventory_id, p.geom
-	FROM inventory_well AS iw
-	JOIN well_place AS wp ON wp.well_id = iw.well_id
-	JOIN place AS p ON p.place_id = wp.place_id
-	WHERE p.geom IS NOT NULL
-
-	UNION ALL
-
-	-- Outcrop PLSS
-	SELECT io.inventory_id, p.geom
-	FROM inventory_outcrop AS io
-	JOIN outcrop_plss AS op ON op.outcrop_id = io.outcrop_id
-	JOIN plss AS p ON p.plss_id = op.plss_id
-	WHERE p.geom IS NOT NULL
-
-	UNION ALL
-
-	-- Well PLSS
-	SELECT iw.inventory_id, p.geom
-	FROM inventory_well AS iw
-	JOIN well_plss AS wp ON wp.well_id = iw.well_id
-	JOIN plss AS p ON p.plss_id = wp.plss_id
-	WHERE p.geom IS NOT NULL
-
-	UNION ALL
-
-	-- Borehole Quadrangle
-	SELECT ib.inventory_id, q.geom
-	FROM inventory_borehole AS ib
-	JOIN borehole_quadrangle AS bq ON bq.borehole_id = ib.borehole_id
-	JOIN quadrangle AS q ON q.quadrangle_id = bq.quadrangle_id
-	WHERE q.geom IS NOT NULL
-
-	UNION ALL
-
-	-- Outcrop Quadrangle
-	SELECT io.inventory_id, q.geom
-	FROM inventory_outcrop AS io
-	JOIN outcrop_quadrangle AS oq ON oq.outcrop_id = io.outcrop_id
-	JOIN quadrangle AS q ON q.quadrangle_id = oq.quadrangle_id
-	WHERE q.geom IS NOT NULL
-
-	UNION ALL
-
-	-- Well Quadrangle
-	SELECT iw.inventory_id, q.geom
-	FROM inventory_well AS iw
-	JOIN well_quadrangle AS wq ON wq.well_id = iw.well_id
-	JOIN quadrangle AS q ON q.quadrangle_id = wq.quadrangle_id
-	WHERE q.geom IS NOT NULL
-
-	UNION ALL
-
-	-- Borehole Mining District
-	SELECT ib.inventory_id, md.geom
-	FROM inventory_borehole AS ib
-	JOIN borehole_mining_district AS bmd ON bmd.borehole_id = ib.borehole_id
-	JOIN mining_district AS md ON md.mining_district_id = bmd.mining_district_id
-	WHERE md.geom IS NOT NULL
-
-	UNION ALL
-
-	-- Outcrop Mining District
-	SELECT io.inventory_id, md.geom
-	FROM inventory_outcrop AS io
-	JOIN outcrop_mining_district AS omd ON omd.outcrop_id = io.outcrop_id
-	JOIN mining_district AS md ON md.mining_district_id = omd.mining_district_id
-	WHERE md.geom IS NOT NULL
+	(
+		-- Borehole Point
+		SELECT ib.inventory_id, p.geom
+		FROM inventory_borehole AS ib
+		JOIN borehole_point AS bp ON bp.borehole_id = ib.borehole_id
+		JOIN point AS p ON p.point_id = bp.point_id
+		WHERE p.geom IS NOT NULL
+	) UNION ALL (
+		-- Outcrop Point
+		SELECT io.inventory_id, p.geom
+		FROM inventory_outcrop AS io
+		JOIN outcrop_point AS op ON op.outcrop_id = io.outcrop_id
+		JOIN point AS p ON p.point_id = op.point_id
+		WHERE p.geom IS NOT NULL
+	) UNION ALL (
+		-- Well Point
+		SELECT iw.inventory_id, p.geom
+		FROM inventory_well AS iw
+		JOIN well_point AS wp ON wp.well_id = iw.well_id
+		JOIN point AS p ON p.point_id = wp.point_id
+		WHERE p.geom IS NOT NULL
+	) UNION ALL (
+		-- Outcrop Place
+		SELECT io.inventory_id, p.geom
+		FROM inventory_outcrop AS io
+		JOIN outcrop_place AS op ON op.outcrop_id = io.outcrop_id
+		JOIN place AS p ON p.place_id = op.place_id
+		WHERE p.geom IS NOT NULL
+	) UNION ALL (
+		-- Well Place
+		SELECT iw.inventory_id, p.geom
+		FROM inventory_well AS iw
+		JOIN well_place AS wp ON wp.well_id = iw.well_id
+		JOIN place AS p ON p.place_id = wp.place_id
+		WHERE p.geom IS NOT NULL
+	) UNION ALL (
+		-- Outcrop PLSS
+		SELECT io.inventory_id, p.geom
+		FROM inventory_outcrop AS io
+		JOIN outcrop_plss AS op ON op.outcrop_id = io.outcrop_id
+		JOIN plss AS p ON p.plss_id = op.plss_id
+		WHERE p.geom IS NOT NULL
+	) UNION ALL (
+		-- Well PLSS
+		SELECT iw.inventory_id, p.geom
+		FROM inventory_well AS iw
+		JOIN well_plss AS wp ON wp.well_id = iw.well_id
+		JOIN plss AS p ON p.plss_id = wp.plss_id
+		WHERE p.geom IS NOT NULL
+	) UNION ALL (
+		-- Borehole Quadrangle
+		SELECT ib.inventory_id, q.geom
+		FROM inventory_borehole AS ib
+		JOIN borehole_quadrangle AS bq ON bq.borehole_id = ib.borehole_id
+		JOIN quadrangle AS q ON q.quadrangle_id = bq.quadrangle_id
+		WHERE q.geom IS NOT NULL
+	) UNION ALL (
+		-- Outcrop Quadrangle
+		SELECT io.inventory_id, q.geom
+		FROM inventory_outcrop AS io
+		JOIN outcrop_quadrangle AS oq ON oq.outcrop_id = io.outcrop_id
+		JOIN quadrangle AS q ON q.quadrangle_id = oq.quadrangle_id
+		WHERE q.geom IS NOT NULL
+	) UNION ALL (
+		-- Well Quadrangle
+		SELECT iw.inventory_id, q.geom
+		FROM inventory_well AS iw
+		JOIN well_quadrangle AS wq ON wq.well_id = iw.well_id
+		JOIN quadrangle AS q ON q.quadrangle_id = wq.quadrangle_id
+		WHERE q.geom IS NOT NULL
+	) UNION ALL (
+		-- Borehole Mining District
+		SELECT ib.inventory_id, md.geom
+		FROM inventory_borehole AS ib
+		JOIN borehole_mining_district AS bmd ON bmd.borehole_id = ib.borehole_id
+		JOIN mining_district AS md ON md.mining_district_id = bmd.mining_district_id
+		WHERE md.geom IS NOT NULL
+	) UNION ALL (
+		-- Outcrop Mining District
+		SELECT io.inventory_id, md.geom
+		FROM inventory_outcrop AS io
+		JOIN outcrop_mining_district AS omd ON omd.outcrop_id = io.outcrop_id
+		JOIN mining_district AS md ON md.mining_district_id = omd.mining_district_id
+		WHERE md.geom IS NOT NULL
+	)
 );
 
 
@@ -228,87 +208,87 @@ CREATE OR REPLACE VIEW inventory_location AS (
 
 
 CREATE OR REPLACE VIEW borehole_geom AS (
-	SELECT bp.borehole_id, po.geom
+	(SELECT bp.borehole_id, po.geom
 	FROM borehole_point AS bp
 	JOIN point AS po ON po.point_id = bp.point_id
-	WHERE po.geom IS NOT NULL
+	WHERE po.geom IS NOT NULL)
 
 	UNION ALL
 
-	SELECT bq.borehole_id, qu.geom
+	(SELECT bq.borehole_id, qu.geom
 	FROM borehole_quadrangle AS bq
 	JOIN quadrangle AS qu ON qu.quadrangle_id = bq.quadrangle_id
-	WHERE qu.geom IS NOT NULL
+	WHERE qu.geom IS NOT NULL)
 	
 	UNION ALL
 	
-	SELECT bm.borehole_id, md.geom
+	(SELECT bm.borehole_id, md.geom
 	FROM borehole_mining_district AS bm
 	JOIN mining_district AS md ON md.mining_district_id = bm.mining_district_id
-	WHERE md.geom IS NOT NULL
+	WHERE md.geom IS NOT NULL)
 );
 
 
 CREATE OR REPLACE VIEW well_geom AS (
-	SELECT wp.well_id, po.geom
-	FROM well_point AS wp
-	JOIN point AS po ON po.point_id = wp.well_id
-	WHERE po.geom IS NOT NULL
-	
-	UNION ALL
-	
-	SELECT wp.well_id, pl.geom
-	FROM well_place AS wp
-	JOIN place AS pl ON pl.place_id = wp.place_id
-	WHERE pl.geom IS NOT NULL
-	
-	UNION ALL
-	
-	SELECT wp.well_id, pl.geom
-	FROM well_plss AS wp
-	JOIN plss AS pl ON pl.plss_id = wp.plss_id
-	WHERE pl.geom IS NOT NULL
-	
-	UNION ALL
-	
-	SELECT wq.well_id, qu.geom
-	FROM well_quadrangle AS wq
-	JOIN quadrangle AS qu ON qu.quadrangle_id = wq.quadrangle_id
-	WHERE qu.geom IS NOT NULL
+	(
+		-- Point
+		SELECT wp.well_id, po.geom
+		FROM well_point AS wp
+		JOIN point AS po ON po.point_id = wp.point_id
+		WHERE po.geom IS NOT NULL
+	) UNION ALL (
+		-- Place
+		SELECT wp.well_id, pl.geom
+		FROM well_place AS wp
+		JOIN place AS pl ON pl.place_id = wp.place_id
+		WHERE pl.geom IS NOT NULL
+	) UNION ALL (
+		-- PLSS
+		SELECT wp.well_id, pl.geom
+		FROM well_plss AS wp
+		JOIN plss AS pl ON pl.plss_id = wp.plss_id
+		WHERE pl.geom IS NOT NULL
+	) UNION ALL (
+		-- Quadrangle
+		SELECT wq.well_id, qu.geom
+		FROM well_quadrangle AS wq
+		JOIN quadrangle AS qu ON qu.quadrangle_id = wq.quadrangle_id
+		WHERE qu.geom IS NOT NULL
+	)
 );
 
 
 CREATE OR REPLACE VIEW outcrop_geom AS (
-	SELECT op.outcrop_id, po.geom
+	(SELECT op.outcrop_id, po.geom
 	FROM outcrop_point AS op
 	JOIN point AS po ON po.point_id = op.point_id
-	WHERE po.geom IS NOT NULL
+	WHERE po.geom IS NOT NULL)
 	
 	UNION ALL
 	
-	SELECT op.outcrop_id, pl.geom
+	(SELECT op.outcrop_id, pl.geom
 	FROM outcrop_place AS op
 	JOIN place AS pl ON pl.place_id = op.place_id
-	WHERE pl.geom IS NOT NULL
+	WHERE pl.geom IS NOT NULL)
 	
 	UNION ALL
 	
-	SELECT op.outcrop_id, pl.geom
+	(SELECT op.outcrop_id, pl.geom
 	FROM outcrop_plss AS op
 	JOIN plss AS pl ON pl.plss_id = op.plss_id
-	WHERE pl.geom IS NOT NULL
+	WHERE pl.geom IS NOT NULL)
 	
 	UNION ALL
 	
-	SELECT oq.outcrop_id, qu.geom
+	(SELECT oq.outcrop_id, qu.geom
 	FROM outcrop_quadrangle AS oq
 	JOIN quadrangle AS qu ON qu.quadrangle_id = oq.quadrangle_id
-	WHERE qu.geom IS NOT NULL
+	WHERE qu.geom IS NOT NULL)
 	
 	UNION ALL
 	
-	SELECT om.outcrop_id, md.geom
+	(SELECT om.outcrop_id, md.geom
 	FROM outcrop_mining_district AS om
 	JOIN mining_district AS md ON md.mining_district_id = om.mining_district_id
-	WHERE md.geom IS NOT NULL
+	WHERE md.geom IS NOT NULL)
 );
