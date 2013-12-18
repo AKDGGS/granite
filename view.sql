1174234ccb04092c3ae8cd30cbd8fda06577a560
Christopher Ramey <christopher.ramey@alaska.gov>
2013-12-18 04:04:36 +0000

    Removed inventory_building and replaced with container_building

--- view.sql
+++ view.sql
@@ -183,30 +183,28 @@ CREATE OR REPLACE VIEW inventory_geom AS (
 );
 
 
-CREATE OR REPLACE VIEW inventory_building AS (
-	SELECT iv.inventory_id, (
-		WITH RECURSIVE t AS (
-			(
-				SELECT co.container_id,	co.parent_container_id,
-					co.name, co.container_type_id
-				FROM inventory_container AS ic
-				JOIN container AS co ON co.container_id = ic.container_id
-				WHERE ic.inventory_id = iv.inventory_id
-				ORDER BY log_date DESC
-				LIMIT 1
-			) UNION ALL (
-				SELECT c.container_id, c.parent_container_id,
-					c.name, c.container_type_id
-				FROM container AS c
-				JOIN t AS t ON c.container_id = t.parent_container_id
-			)
-		) SELECT container_id FROM t
-		WHERE container_type_id = 4
-			OR container_type_id = 9
-			OR container_type_id = 10
-		LIMIT 1
-	) AS container_id
-	FROM inventory AS iv
+CREATE OR REPLACE VIEW container_building AS ( 
+	WITH RECURSIVE t AS (
+		(
+			SELECT c.container_id, c.container_id AS building_id,
+				c.parent_container_id, c.container_type_id
+			FROM container AS c
+		) UNION ALL (
+			SELECT t.container_id, c.container_id AS building_id,
+				c.parent_container_id, c.container_type_id
+			FROM container AS c
+			JOIN t ON t.parent_container_id = c.container_id
+		)
+	)
+	SELECT container_id, building_id
+	FROM t
+	WHERE container_type_id IN (
+		SELECT container_type_id
+		FROM container_type
+		WHERE name = 'connex'
+			OR name = 'out building'
+			OR name = 'main warehouse'
+	)
 );
 
 
