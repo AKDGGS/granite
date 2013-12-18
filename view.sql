61b3d48232f177958ddfd1b292852e05df2f7569
Christopher Ramey <christopher.ramey@alaska.gov>
2013-12-18 00:43:40 +0000

    Added view for querying inventory's building

--- view.sql
+++ view.sql
@@ -183,6 +183,33 @@ CREATE OR REPLACE VIEW inventory_geom AS (
 );
 
 
+CREATE OR REPLACE VIEW inventory_building AS (
+	SELECT iv.inventory_id, (
+		WITH RECURSIVE t AS (
+			(
+				SELECT co.container_id,	co.parent_container_id,
+					co.name, co.container_type_id
+				FROM inventory_container AS ic
+				JOIN container AS co ON co.container_id = ic.container_id
+				WHERE ic.inventory_id = iv.inventory_id
+				ORDER BY log_date DESC
+				LIMIT 1
+			) UNION ALL (
+				SELECT c.container_id, c.parent_container_id,
+					c.name, c.container_type_id
+				FROM container AS c
+				JOIN t AS t ON c.container_id = t.parent_container_id
+			)
+		) SELECT container_id FROM t
+		WHERE container_type_id = 4
+			OR container_type_id = 9
+			OR container_type_id = 10
+		LIMIT 1
+	) AS container_id
+	FROM inventory AS iv
+);
+
+
 CREATE OR REPLACE VIEW inventory_location AS (
 	SELECT iv.inventory_id, (
 		SELECT STRING_AGG(name, '/') FROM (
@@ -199,7 +226,8 @@ CREATE OR REPLACE VIEW inventory_location AS (
 					SELECT level+1, c.container_id,
 						c.parent_container_id, c.name
 					FROM container AS c
-					JOIN t AS t ON c.container_id = t.parent_container_id)
+					JOIN t AS t ON c.container_id = t.parent_container_id
+				)
 			) SELECT name FROM t ORDER BY level DESC
 		) AS tree
 	) AS location
