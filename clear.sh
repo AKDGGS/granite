a880b572aac9f48852989a5493824b88d027aea3
Christopher Ramey <christopher.ramey@alaska.gov>
2014-03-07 18:42:58 +0000

    Added sanity checking to clear script so that failures keep it from proceeding

--- clear.sh
+++ clear.sh
@@ -1,7 +1,25 @@
 #1/bin/sh
 DATABASE=gmc
-/usr/bin/psql -d $DATABASE -f gmc.sql
-/usr/bin/psql -d $DATABASE -f trigger.sql
-/usr/bin/psql -d $DATABASE -f index.sql
-/usr/bin/psql -d $DATABASE -f view.sql
-/usr/bin/psql -d $DATABASE -c "VACUUM FULL ANALYZE"
+
+/usr/bin/psql -v ON_ERROR_STOP=1 -d $DATABASE -f gmc.sql
+RETVAL=$?
+
+if [ $RETVAL == 0 ]; then
+	/usr/bin/psql -v ON_ERROR_STOP=1 -d $DATABASE -f trigger.sql
+	RETVAL=$?
+fi
+
+if [ $RETVAL == 0 ]; then
+	/usr/bin/psql -v ON_ERROR_STOP=1 -d $DATABASE -f index.sql
+	RETVAL=$?
+fi
+
+if [ $RETVAL == 0 ]; then
+	/usr/bin/psql -v ON_ERROR_STOP=1 -d $DATABASE -f view.sql
+	RETVAL=$?
+fi
+
+if [ $RETVAL == 0 ]; then
+	/usr/bin/psql -v ON_ERROR_STOP=1 -d $DATABASE -c "VACUUM FULL ANALYZE"
+	RETVAL=$?
+fi
