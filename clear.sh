9c97a3168564c13b77fa911db14e7a468a682d90
Christopher Ramey <christopher.ramey@alaska.gov>
2014-04-24 19:41:11 +0000

    Added permissions sql to clear script

--- clear.sh
+++ clear.sh
@@ -29,6 +29,11 @@ if [ $RETVAL == 0 ]; then
 fi
 
 if [ $RETVAL == 0 ]; then
+	/usr/bin/psql -v ON_ERROR_STOP=1 -d $DATABASE -f permissions.sql
+	RETVAL=$?
+fi
+
+if [ $RETVAL == 0 ]; then
 	/usr/bin/psql -v ON_ERROR_STOP=1 -d $DATABASE -c "VACUUM FULL ANALYZE"
 	RETVAL=$?
 fi
