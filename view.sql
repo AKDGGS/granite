d77f5a8297890ceb8b4580b24f3445ce8fbc8ee3
Christopher Ramey <christopher.ramey@alaska.gov>
2014-05-25 00:05:14 +0000

    Added view for organizing inventory into "bins" for the move. Removed all the "OR REPLACE" from the view create SQL, since replaced views are expected to return the same columns and types as before. Better to destroy and re-create.

--- view.sql
+++ view.sql
@@ -1,8 +1,8 @@
 SET SCHEMA 'public';
 SET CLIENT_MIN_MESSAGES TO WARNING;
 
-
-CREATE OR REPLACE VIEW container_building AS ( 
+DROP VIEW IF EXISTS container_building;
+CREATE VIEW container_building AS ( 
 	WITH RECURSIVE t AS (
 		(
 			SELECT c.container_id, c.container_id AS building_id,
@@ -27,7 +27,8 @@ CREATE OR REPLACE VIEW container_building AS (
 );
 
 
-CREATE OR REPLACE VIEW container_path AS (
+DROP VIEW IF EXISTS container_path;
+CREATE VIEW container_path AS (
 	WITH RECURSIVE t AS (
 		(
 			SELECT 0 AS level, c1.name,
@@ -47,14 +48,16 @@ CREATE OR REPLACE VIEW container_path AS (
 );
 
 
-CREATE OR REPLACE VIEW inventory_shotline AS (
+DROP VIEW IF EXISTS inventory_shotline;
+CREATE VIEW inventory_shotline AS (
 	SELECT DISTINCT isp.inventory_id, sp.shotline_id
 	FROM inventory_shotpoint AS isp
 	JOIN shotpoint AS sp ON sp.shotpoint_id = isp.shotpoint_id
 );
 
 
-CREATE OR REPLACE VIEW inventory_shotline_minmax AS (
+DROP VIEW IF EXISTS inventory_shotline_minmax;
+CREATE VIEW inventory_shotline_minmax AS (
 	SELECT isp.inventory_id, sp.shotline_id,
 		MIN(sp.shotpoint_number) AS shotline_min,
 		MAX(sp.shotpoint_number) AS shotline_max
@@ -63,4 +66,28 @@ CREATE OR REPLACE VIEW inventory_shotline_minmax AS (
 		ON isp.shotpoint_id = sp.shotpoint_id
 	WHERE sp.shotpoint_number IS NOT NULL
 	GROUP BY isp.inventory_id, sp.shotline_id
+);
+
+DROP VIEW IF EXISTS inventory_bin;
+CREATE VIEW inventory_bin AS (
+	SELECT i.inventory_id, CASE
+		WHEN i.radiation_cps > 0 OR i.radiation_msv > 0 THEN 'Radioactive'
+		WHEN il.inventory_id IS NOT NULL THEN 'Light-weight'
+		ELSE COALESCE(ig.name, 'Unknown') END AS bin
+	FROM inventory AS i
+	LEFT OUTER JOIN (
+		SELECT DISTINCT ik.inventory_id
+		FROM inventory_keyword AS ik
+		JOIN keyword AS k ON k.keyword_id = ik.keyword_id
+		WHERE k.name IN (
+			'plug','washed','vial',
+			'crude','chips','seep'
+		)
+	) AS il ON il.inventory_id = i.inventory_id
+	LEFT OUTER JOIN (
+		SELECT DISTINCT ON (inventory_id) ig.inventory_id, g.name
+		FROM inventory_geog AS ig
+		JOIN gmc_region_geog AS gg ON ST_Intersects(gg.geog, ig.geog)
+		JOIN gmc_region AS g ON g.gmc_region_id = gg.gmc_region_id
+	) AS ig ON ig.inventory_id = i.inventory_id
 );
