5d8b375b73c85d095ea463428cb567ea1909d45a
Christopher Ramey <christopher.ramey@alaska.gov>
2015-02-01 22:14:10 +0000

    Added function to create container paths

--- functions.sql
+++ functions.sql
@@ -77,3 +77,72 @@ CREATE FUNCTION public.mk_wellpoint(
 		RETURN pid;
 	END
 $$ LANGUAGE plpgsql;
+
+
+-- Makes a container path, that is, ANC/01/02 creates three containers,
+-- with three paths. The barcode is attached to the final of the
+-- created path elements.
+-- Takes three parameters: An array of container names, an array
+-- of container types (which it creates if they aren't found
+-- and a barcode.
+DROP FUNCTION IF EXISTS public.mk_container_path(VARCHAR[], VARCHAR[], VARCHAR);
+CREATE FUNCTION public.mk_container_path(
+	paths VARCHAR[], types VARCHAR[], barcode VARCHAR
+) RETURNS int AS $$
+	DECLARE
+		pid INT := NULL;
+		cid INT;
+		plen INT;
+		tid INT;
+	BEGIN
+		plen := ARRAY_LENGTH(paths, 1);
+		
+		-- Die if the array lengths don't match
+		IF plen <> ARRAY_LENGTH(types, 1) THEN
+			RAISE EXCEPTION 'Path and type lengths are not the same.';
+		END IF;
+		
+		-- Loop over container path
+		FOR i IN 1 .. plen LOOP
+			IF pid IS NULL THEN
+				SELECT container_id INTO cid
+				FROM container
+				WHERE LOWER(name) = LOWER(paths[i])
+					AND parent_container_id IS NULL
+				LIMIT 1;
+			ELSE	
+				SELECT container_id INTO cid
+				FROM container
+				WHERE LOWER(name) = LOWER(paths[i])
+					AND parent_container_id = pid
+				LIMIT 1;
+			END IF;
+
+			IF NOT FOUND THEN
+				-- Find the type
+				SELECT container_type_id INTO tid
+				FROM container_type
+				WHERE LOWER(name) = LOWER(types[i])
+				LIMIT 1;
+				-- or insert as needed
+				IF NOT FOUND THEN
+					INSERT INTO container_type (name) VALUES (types[i])
+					RETURNING container_type_id INTO tid;
+				END IF;
+			
+				INSERT INTO container (
+					name, parent_container_id, container_type_id,
+					barcode
+				) VALUES (
+					paths[i], pid, tid,
+					CASE WHEN i = plen THEN barcode ELSE NULL END
+				) RETURNING container_id INTO cid;
+			END IF;
+
+			-- Set parent_id to current container_id for next iteration
+			pid := cid;
+		END LOOP;
+
+		RETURN pid;
+	END
+$$ LANGUAGE plpgsql;
