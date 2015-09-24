e57e48555c3871fd93cfc567a86e61bfb713bd34
Christopher Ramey <christopher.ramey@alaska.gov>
2015-09-24 18:09:41 +0000

    Made the modified_user update function a little more generic

--- trigger.sql
+++ trigger.sql
@@ -84,7 +84,7 @@ FOR EACH ROW EXECUTE PROCEDURE modified_date_fn();
 
 
 -- Create function for inventory modified user touching on update/insert
-CREATE OR REPLACE FUNCTION inventory_modified_user_fn()
+CREATE OR REPLACE FUNCTION modified_user_fn()
 RETURNS TRIGGER AS $$
 BEGIN
 	NEW.modified_user = session_user;
@@ -94,7 +94,7 @@ END; $$ language 'plpgsql';
 -- Set trigger for inventory modified user
 DROP TRIGGER IF EXISTS inventory_modified_user_tr ON inventory;
 CREATE TRIGGER inventory_modified_user_tr BEFORE INSERT OR UPDATE ON inventory
-FOR EACH ROW EXECUTE PROCEDURE inventory_modified_user_fn();
+FOR EACH ROW EXECUTE PROCEDURE modified_user_fn();
 
 
 COMMIT;
